85f8bf663e091912677b6c72ae5453d7e9062b48
singcl <1276837689@qq.com>
2022-10-04 01:44:30 +0800

    feat(md): roadmap

--- src-tauri/src/command/m3u8.rs
+++ src-tauri/src/command/m3u8.rs
@@ -17,12 +17,14 @@ pub(crate) async fn m3u8_download(
     save_path: String,
     m3u8_url: &str,
 ) -> Result<String, error::M3u8Error> {
+    // TODO: 固定设置XmVideoPlayer的数据目录
     let temp_dir = PathBuf::from(format!("{}/temp", save_path));
     let temp_dir_str = temp_dir.to_str().unwrap();
     std::fs::create_dir_all(&temp_dir).unwrap();
 
     let url_list = request::get_m3u8_list(m3u8_url).await?;
     let url_list_entity = parse::parse_m3u8_list(&url_list, m3u8_url);
+    // TODO:断点续下
     request::get_all_ts(&url_list_entity, temp_dir_str).await;
     merge::merge_ts(temp_dir_str);
 
