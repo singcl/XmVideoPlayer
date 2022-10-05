89707588ef97deda11553b22b82177e61f1a6e18
singcl <1276837689@qq.com>
2022-10-05 18:31:42 +0800

    fix(refactor):optimize merge stream!

--- src-tauri/src/command/m3u8/merge.rs
+++ src-tauri/src/command/m3u8/merge.rs
@@ -3,17 +3,6 @@ use std::fs::File;
 use std::path::PathBuf;
 
 pub fn merge_ts(url_list_entity_hash: &Vec<u64>, temp_dir: &str, out_path: &str) {
-    // 读取目录下所有文件路径
-    // 可能包含其他非ts文件 如.DS_Store
-    // TODO:过滤掉无关的文件
-    let mut paths = std::fs::read_dir(temp_dir)
-        .unwrap()
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, std::io::Error>>()
-        .unwrap();
-    // 非数字顺序
-    paths.sort();
-
     // let out_path = PathBuf::from(temp_dir);
     // let mut out_path = PathBuf::from(out_path.parent().unwrap());
     // out_path.push("output.ts");
