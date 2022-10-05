bb695a115157303e514185fd8e0f19ef5f92ca80
singcl <1276837689@qq.com>
2022-10-05 22:06:08 +0800

    feature(core):断点继续下载complete!

--- src-tauri/src/command/m3u8.rs
+++ src-tauri/src/command/m3u8.rs
@@ -42,21 +42,21 @@ pub(crate) async fn m3u8_download(
     let url_list = request::get_m3u8_list(m3u8_url).await?;
     let url_list_entity = parse::parse_m3u8_list(&url_list, m3u8_url);
     
-    let mut url_list_entity_hash = url_list_entity
+    let url_list_entity_hash = url_list_entity
         .iter()
         .map(|r| utils::hash_str(r))
         .collect::<Vec<_>>();
 
-    url_list_entity_hash = utils::entity_hash_filter(&url_list_entity_hash, temp_dir_str);
-    let start = url_list_entity.len() - url_list_entity_hash.len();
+    let url_list_entity_hash_dl = utils::entity_hash_filter(&url_list_entity_hash, temp_dir_str);
+    let start = url_list_entity.len() - url_list_entity_hash_dl.len();
 
-    // println!("---{:?}", url_list_entity_hash);
-    // println!("---{:?}", start);
+    println!("---{:?}", url_list_entity_hash_dl);
+    println!("---{:?}", start);
 
     // TODO:断点续下
     request::get_all_ts(
         &url_list_entity,
-        &url_list_entity_hash,
+        &url_list_entity_hash_dl,
         start,
         temp_dir_str,
         &window,
