3f9105e497a7e8772092db0100a812797dfde667
singcl <1276837689@qq.com>
2022-10-15 12:09:56 +0800

    feat(db): initial data in upgrade callback

--- src/database/db.ts
+++ src/database/db.ts
@@ -8,18 +8,18 @@ export interface PlayHistory {
   name: string;
 }
 
-export interface InitialTable {
-  id?: number;
-  name: string;
-  initial: boolean;
-}
+// export interface InitialTable {
+//   id?: number;
+//   name: string;
+//   initial: boolean;
+// }
 
 export enum XM_DB {
   PLAYER_DB = 'player',
 }
 
 export enum XM_TABLE {
-  INITIAL_TABLE = 'initial',
+  // INITIAL_TABLE = 'initial',
   PLAY_HISTORY_TABLE = 'play_history',
 }
 
@@ -27,20 +27,17 @@ class MySubClassedDexie extends Dexie {
   // 'friends' is added by dexie when declaring the stores()
   // We just tell the typing system this is the case
   [XM_TABLE.PLAY_HISTORY_TABLE]!: Table<PlayHistory>;
-  [XM_TABLE.INITIAL_TABLE]!: Table<InitialTable>;
+  // [XM_TABLE.INITIAL_TABLE]!: Table<InitialTable>;
   constructor() {
     super(XM_DB['PLAYER_DB']);
-    this.version(1).stores({
-      [XM_TABLE.PLAY_HISTORY_TABLE]: '++id, &name, url', // Primary key and indexed props
-      [XM_TABLE.INITIAL_TABLE]: '++id, &name, initialized', // Primary key and indexed props
-    });
-  }
-  async initializeHistory() {
-    const initTable: Table<InitialTable> = db.table(XM_TABLE.INITIAL_TABLE);
-    // 插入成功则表明是第一次插入
-    await initTable.add({ name: XM_TABLE.PLAY_HISTORY_TABLE, initial: true });
-    const historyTable: Table<PlayHistory> = db.table(XM_TABLE.PLAY_HISTORY_TABLE);
-    await historyTable.bulkAdd(historyListDefault);
+    this.version(1)
+      .stores({
+        [XM_TABLE.PLAY_HISTORY_TABLE]: '++id, &name, url', // Primary key and indexed props
+        // [XM_TABLE.INITIAL_TABLE]: '++id, &name, initialized', // Primary key and indexed props
+      })
+      .upgrade((trans) => {
+        return trans.table<PlayHistory>(XM_TABLE.PLAY_HISTORY_TABLE).bulkAdd(historyListDefault);
+      });
   }
 }
 
