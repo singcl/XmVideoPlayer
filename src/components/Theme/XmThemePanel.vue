991cccddece94df9763e17f831636cb2c586c56a
singcl <1276837689@qq.com>
2022-10-19 17:54:09 +0800

    feat(component): theme select component

--- src/components/Theme/XmThemePanel.vue
+++ src/components/Theme/XmThemePanel.vue
@@ -7,15 +7,18 @@
       </template>
       <template #content>
         <a-radio-group>
-          <template v-for="item in 2" :key="item">
-            <a-radio :value="item">
+          <template v-for="theme in themes" :key="theme.value">
+            <a-radio :value="theme.value">
               <template #radio="{ checked }">
-                <div class="custom-radio-card" :class="{ 'custom-radio-card-checked': checked }">
-                  <div className="custom-radio-card-mask">
-                    <div className="custom-radio-card-mask-dot" />
+                <div
+                  class="theme-radio-card"
+                  :class="[{ 'theme-radio-card-checked': checked }, `theme-${theme.value}`]"
+                >
+                  <div className="theme-radio-card-mask">
+                    <div className="theme-radio-card-mask-dot" />
                   </div>
                   <div>
-                    <a-typography-text type="secondary">金秋黄色</a-typography-text>
+                    <a-typography-text type="secondary">{{ theme.label }}</a-typography-text>
                   </div>
                 </div>
               </template>
@@ -27,7 +30,22 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const themes = [
+  {
+    label: '深海蓝',
+    value: 'blue',
+  },
+  {
+    label: '喜庆红',
+    value: 'red',
+  },
+  {
+    label: '清新绿',
+    value: 'green',
+  },
+];
+</script>
 
 <style scoped>
 .theme-wrapper {
@@ -50,15 +68,21 @@
   margin-bottom: 3px;
 }
 
-.theme-popover .arco-radio-group .arco-radio {
-  margin-right: 0;
+.theme-popover .arco-radio-group {
+  display: flex;
+  flex-direction: column;
 }
 
-.theme-popover .arco-radio-group .arco-radio:first-child {
+.theme-popover .arco-radio-group .arco-radio {
+  margin-right: 0;
   padding-left: 0;
 }
 
-.theme-popover .custom-radio-card {
+.theme-popover .arco-radio-group .arco-radio:not(:last-child) {
+  margin-bottom: 5px;
+}
+
+.theme-popover .theme-radio-card {
   padding: 10px 16px;
   border: 1px solid var(--color-border-2);
   border-radius: 4px;
@@ -66,7 +90,7 @@
   box-sizing: border-box;
 }
 
-.theme-popover .custom-radio-card-mask {
+.theme-popover .theme-radio-card-mask {
   height: 14px;
   width: 14px;
   display: inline-flex;
@@ -77,24 +101,64 @@
   box-sizing: border-box;
 }
 
-.theme-popover .custom-radio-card-mask-dot {
+.theme-popover .theme-radio-card-mask-dot {
   width: 8px;
   height: 8px;
   border-radius: 100%;
 }
 
-.custom-radio-card:hover,
-.custom-radio-card-checked,
-.custom-radio-card:hover .custom-radio-card-mask,
-.custom-radio-card-checked .custom-radio-card-mask {
+.theme-popover .theme-radio-card.theme-blue {
+  background: linear-gradient(91deg, rgb(var(--blue-1)), rgb(var(--blue-5)) 70%, rgb(var(--blue-7)));
+}
+.theme-popover .theme-radio-card.theme-red {
+  background: linear-gradient(91deg, rgb(var(--red-1)), rgb(var(--red-5)) 70%, rgb(var(--red-7)));
+}
+.theme-popover .theme-radio-card.theme-green {
+  background: linear-gradient(91deg, rgb(var(--green-1)), rgb(var(--green-5)) 70%, rgb(var(--green-7)));
+}
+
+.theme-popover .theme-blue.theme-radio-card:hover,
+.theme-popover .theme-blue.theme-radio-card-checked,
+.theme-popover .theme-blue.theme-radio-card:hover .theme-radio-card-mask,
+.theme-popover .theme-blue.theme-radio-card-checked .theme-radio-card-mask {
   border-color: rgb(var(--primary-6));
 }
 
-.custom-radio-card-checked {
+.theme-popover .theme-blue.theme-radio-card-checked {
   background-color: var(--color-primary-light-1);
 }
 
-.custom-radio-card-checked .custom-radio-card-mask-dot {
+.theme-popover .theme-blue.theme-radio-card-checked .theme-radio-card-mask-dot {
   background-color: rgb(var(--primary-6));
+}
+
+.theme-popover .theme-red.theme-radio-card:hover,
+.theme-popover .theme-red.theme-radio-card-checked,
+.theme-popover .theme-red.theme-radio-card:hover .theme-radio-card-mask,
+.theme-popover .theme-red.theme-radio-card-checked .theme-radio-card-mask {
+  border-color: rgb(var(--danger-6));
+}
+
+.theme-popover .theme-red.theme-radio-card-checked {
+  background-color: var(--color-danger-light-1);
+}
+
+.theme-popover .theme-red.theme-radio-card-checked .theme-radio-card-mask-dot {
+  background-color: rgb(var(--danger-6));
+}
+
+.theme-popover .theme-green.theme-radio-card:hover,
+.theme-popover .theme-green.theme-radio-card-checked,
+.theme-popover .theme-green.theme-radio-card:hover .theme-radio-card-mask,
+.theme-popover .theme-green.theme-radio-card-checked .theme-radio-card-mask {
+  border-color: rgb(var(--green-6));
+}
+
+.theme-popover .theme-green.theme-radio-card-checked {
+  background-color: var(--color-green-light-1);
+}
+
+.theme-popover .theme-green.theme-radio-card-checked .theme-radio-card-mask-dot {
+  background-color: rgb(var(--green-6));
 }
 </style>
