73551818ffd0f183bebc53e485a08f32e6a3d22e
singcl <1276837689@qq.com>
2022-11-03 15:49:21 +0800

    chore(stylelint): postcss settings

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  // plugins: {
+  //   'postcss-sorting': {
+  //     order: ['custom-properties', 'dollar-variables', 'declarations', 'at-rules', 'rules'],
+  //     'properties-order': 'alphabetical',
+  //     'unspecified-properties-position': 'bottom',
+  //   },
+  // },
+};
