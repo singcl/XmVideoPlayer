2f36fe95cadf9f1e7f86ed4e2bef7f50c696180f
singcl <1276837689@qq.com>
2022-11-03 15:09:18 +0800

    chore(stylelint): config stylelint

---
+++ stylelint.config.cjs
@@ -0,0 +1,34 @@
+// @see https://www.npmjs.com/package/stylelint-stylus
+// @see https://stylelint.io/user-guide/configure/
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    // 'stylelint-config-standard-scss',
+    // 'stylelint-config-prettier-scss',
+    // @see https://www.npmjs.com/package/stylelint-stylus
+    // 'stylelint-stylus/standard',
+    // @see https://www.npmjs.com/package/postcss-html
+    'stylelint-config-html',
+    // If you are using Vue.
+    'stylelint-config-recommended-vue',
+  ],
+  // plugins: [
+  //   // add this plugin here:
+  //   "stylelint-stylus",
+  // ],
+  // overrides: [
+  //   {
+  //     files: ["*.stylus", "*.styl", "**/*.stylus", "**/*.styl"],
+  //     customSyntax: "postcss-styl",
+  //   },
+  // ],
+  rules: {
+    // override/add rules settings here, such as:
+    // "stylus/declaration-colon": "never"
+    // @see https://stylelint.io/user-guide/rules/list/selector-pseudo-class-no-unknown/#ignorepseudoclasses-regex-string
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'local'] }],
+    'value-keyword-case': ['lower', { camelCaseSvgKeywords: true }],
+    'property-no-vendor-prefix': [true, { ignoreProperties: ['text-size-adjust'] }],
+  },
+};
