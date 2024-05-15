14825b8f7370413caee238ec98dd3cff50437adc
singcl <1276837689@qq.com>
2024-05-16 00:14:28 +0800

    feat(core): test ui group

--- src/components/HTest.vue
+++ src/components/HTest.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="container">
-    <a-button :loading="loading" type="primary" @click="handleTestClick">下载测试按钮</a-button>
-    <a-button :loading="loading" type="primary" @click="handleTestClick2">全屏测试按钮</a-button>
-    <a-button :loading="loading" type="primary" @click="handleTestClick3">pinia测试按钮</a-button>
+    <a-space>
+      <a-button :loading="loading" type="primary" @click="handleDownloadClick">下载测试按钮</a-button>
+      <a-button :loading="loading" type="primary" @click="handleFullClick">全屏测试按钮</a-button>
+      <a-button :loading="loading" type="primary" @click="handlePiniaClick">pinia测试按钮</a-button>
+    </a-space>
   </div>
 </template>
 
@@ -19,7 +21,7 @@ import { useHeightStore } from '@/stores';
 const loading = ref(false);
 const heightStore = useHeightStore();
 // 测试按钮
-async function handleTestClick() {
+async function handleDownloadClick() {
   // const url = convertFileSrc('https://media.w3.org/2010/05/sintel/trailer.mp4', 'stream');
   // console.log('---url', url);
   // const response = await fetch(url, {
@@ -28,25 +30,28 @@ async function handleTestClick() {
   // console.log('--response', response);
   try {
     const downloadDirPath = await downloadDir();
-    const filePath = await save({
-      // TODO: 这个filters什么意思？？
-      // filters: [
-      //   {
-      //     name: 'Video',
-      //     extensions: ['mp4'],
-      //   },
-      //   {
-      //     name: 'Image',
-      //     extensions: ['png', 'jpg', 'jpeg'],
-      //   },
-      // ],
-      defaultPath: downloadDirPath,
-    });
-    if (!filePath) return;
+    // const filePath = await save({
+    //   // TODO: 这个filters什么意思？？
+    //   // filters: [
+    //   //   {
+    //   //     name: 'Video',
+    //   //     extensions: ['mp4'],
+    //   //   },
+    //   //   {
+    //   //     name: 'Image',
+    //   //     extensions: ['png', 'jpg', 'jpeg'],
+    //   //   },
+    //   // ],
+    //   defaultPath: downloadDirPath,
+    // });
+    // if (!filePath) return;
+    const filePath = 'C:\\Users\\Administrator\\Downloads\\1.txt';
     console.log('------', filePath);
-    loading.value = true;
+    // loading.value = true;
     const res = await invoke('m3u8_download', {
-      m3u8Url: 'https://bitdash-a.akamaihd.net/content/sintel/hls/video/800kbit.m3u8',
+      // m3u8Url: 'https://bitdash-a.akamaihd.net/content/sintel/hls/video/800kbit.m3u8',
+      m3u8Url:
+        'https://hls.vdtuzv.com/videos3/8f51051627782468d307693477a97498/8f51051627782468d307693477a97498.m3u8?auth_key=1715787570-6644d732a0209-0-2bd3ed08e744a57ea18c04925bdc01ce&v=3&time=0',
       savePath: filePath,
     });
     console.log('------', res);
@@ -55,11 +60,11 @@ async function handleTestClick() {
   }
 }
 
-async function handleTestClick2() {
+async function handleFullClick() {
   const full = await appWindow.isFullscreen();
   await appWindow.setFullscreen(!full);
 }
-async function handleTestClick3() {
+async function handlePiniaClick() {
   heightStore.$patch({ height: '50vh' });
 }
 </script>
