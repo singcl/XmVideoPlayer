5a303261950be3d3f26eadcb1a8e024698553b51
singcl <1276837689@qq.com>
2024-06-02 13:23:47 +0800

    feat(core): views update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,29 +6,30 @@
     </h1>
     <XmSearch />
     <a-list
-      class="list-demo-action-layout"
+      ref="listRef"
+      class="his-list-action-layout"
       :bordered="false"
-      :data="dataSource"
-      :pagination-props="paginationProps"
+      :data="dataSource?.list || []"
       :scrollbar="true"
-      :max-height="400"
+      :virtual-list-props="{ height: 'calc(100vh - 160px)' }"
     >
       <template #item="{ item }">
-        <a-list-item class="list-demo-item" action-layout="vertical">
+        <a-list-item class="his-list-item" action-layout="vertical">
           <template #actions>
             <span><icon-heart />83</span>
             <span><icon-star />{{ item.index }}</span>
             <span><icon-message />Reply</span>
           </template>
-          <template #extra>
+          <!-- <template #extra>
             <div className="image-area">
               <img alt="arco-design" :src="item.imageSrc" />
             </div>
-          </template>
-          <a-list-item-meta :title="item.title" :description="item.description">
+          </template> -->
+          <a-list-item-meta :title="item.name" :description="item.url">
             <template #avatar>
               <a-avatar shape="square">
-                <img alt="avatar" :src="item.avatar" />
+                <!-- <img alt="avatar" :src="item.name" /> -->
+                {{ item.name?.slice(0, 1) }}
               </a-avatar>
             </template>
           </a-list-item-meta>
@@ -43,32 +44,22 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 const APP_TITLE = import.meta.env.VITE_APP_TITLE;
 
-import { reactive } from 'vue';
-
-const names = ['Socrates', 'Balzac', 'Plato'];
-const avatarSrc = [
-  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a8c8cdb109cb051163646151a4a5083b.png~tplv-uwbnlip3yd-webp.webp',
-  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/e278888093bef8910e829486fb45dd69.png~tplv-uwbnlip3yd-webp.webp',
-  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/9eeb1800d9b78349b24682c3518ac4a3.png~tplv-uwbnlip3yd-webp.webp',
-];
-const imageSrc = [
-  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/29c1f9d7d17c503c5d7bf4e538cb7c4f.png~tplv-uwbnlip3yd-webp.webp',
-  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/04d7bc31dd67dcdf380bc3f6aa07599f.png~tplv-uwbnlip3yd-webp.webp',
-  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/1f61854a849a076318ed527c8fca1bbf.png~tplv-uwbnlip3yd-webp.webp',
-];
-const dataSource = new Array(15).fill(null).map((_, index) => {
-  return {
-    index: index,
-    avatar: avatarSrc[index % avatarSrc.length],
-    title: names[index % names.length],
-    description:
-      'Beijing ByteDance Technology Co., Ltd. is an enterprise located in China. ByteDance has products such as TikTok, Toutiao, volcano video and Douyin (the Chinese version of TikTok).',
-    imageSrc: imageSrc[index % imageSrc.length],
-  };
-});
+import API from '@/api';
+import { useObservable, from } from '@vueuse/rxjs';
+import { liveQuery } from 'dexie';
+import { onMounted, reactive, ref } from 'vue';
+//
+const dataSource = useObservable(
+  from(
+    liveQuery(async () => {
+      const r = await API.idb.getPlayerHistoryPageList();
+      return r.data;
+    })
+  )
+);
 const paginationProps = reactive({
   defaultPageSize: 3,
-  total: dataSource.length,
+  total: dataSource.value?.list.length,
 });
 </script>
 
@@ -115,23 +106,25 @@ const paginationProps = reactive({
 </style>
 
 <style scoped>
-.list-demo-action-layout .image-area {
+.his-list-action-layout .image-area {
   overflow: hidden;
   width: 183px;
   height: 119px;
   border-radius: 2px;
 }
 
-.list-demo-action-layout .list-demo-item {
+.his-list-action-layout .his-list-item {
   padding: 20px 0;
-  border-bottom: 1px solid var(--color-fill-3);
+  border-bottom: 1px solid var(--xm-color-border-3);
+  text-align: left;
+  word-break: break-all;
 }
 
-.list-demo-action-layout .image-area img {
+.his-list-action-layout .image-area img {
   width: 100%;
 }
 
-.list-demo-action-layout .arco-list-item-action .arco-icon {
+.his-list-action-layout .arco-list-item-action .arco-icon {
   margin: 0 4px;
 }
 </style>
