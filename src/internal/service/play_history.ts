4319a6fba5ded527361f275f1828e9cd86670a17
singcl <1276837689@qq.com>
2024-06-02 13:22:25 +0800

    refactor(core): refactor

--- src/internal/service/play_history.ts
+++ src/internal/service/play_history.ts
@@ -8,19 +8,19 @@ export interface UpdatePlayHistory extends Partial<PlayHistory> {
 }
 //
 const db = DexieDb.create();
+const dbTable = db[XM_TABLE.PLAY_HISTORY_TABLE];
 
 export function queryHistoryList() {
-  return db.transaction('r', [db[XM_TABLE.PLAY_HISTORY_TABLE]], async () => {
-    const list = await db[XM_TABLE.PLAY_HISTORY_TABLE].toArray();
+  return db.transaction('r', [dbTable], async () => {
+    const list = await dbTable.toArray();
     return list;
   });
 }
 
 export function queryHistoryPageList(params?: { page?: PageCamels }) {
   const { page: { pageNo, pageSize } = { pageNo: 1, pageSize: 20 } } = params ?? {};
-  return db.transaction('r', [db[XM_TABLE.PLAY_HISTORY_TABLE]], async () => {
+  return db.transaction('r', [dbTable], async () => {
     const offset = (pageNo - 1) * pageSize;
-    const dbTable = db[XM_TABLE.PLAY_HISTORY_TABLE];
     const count = await dbTable.count();
     const list = await dbTable.offset(offset).limit(pageSize).toArray();
     return { list, total: count, pageNo, pageSize };
@@ -28,15 +28,15 @@ export function queryHistoryPageList(params?: { page?: PageCamels }) {
 }
 
 export function deleteHistory(id: number) {
-  return db.transaction('rw', [db[XM_TABLE.PLAY_HISTORY_TABLE]], async () => {
-    await db[XM_TABLE.PLAY_HISTORY_TABLE].delete(id);
+  return db.transaction('rw', [dbTable], async () => {
+    await dbTable.delete(id);
     return null;
   });
 }
 
 export function saveHistory(data: SavePlayHistory) {
-  return db.transaction('rw', [db[XM_TABLE.PLAY_HISTORY_TABLE]], async () => {
-    const record = await db[XM_TABLE.PLAY_HISTORY_TABLE].where('url').equals(data.url).first();
+  return db.transaction('rw', [dbTable], async () => {
+    const record = await dbTable.where('url').equals(data.url).first();
     if (record) return record.id;
     const res = await db.table<SavePlayHistory, number>(XM_TABLE.PLAY_HISTORY_TABLE).add(data);
     return res;
@@ -44,8 +44,8 @@ export function saveHistory(data: SavePlayHistory) {
 }
 
 export function updateHistory(data: UpdatePlayHistory) {
-  return db.transaction('rw', [db[XM_TABLE.PLAY_HISTORY_TABLE]], async () => {
-    const record = await db[XM_TABLE.PLAY_HISTORY_TABLE].where('id').equals(data.id).modify(data);
+  return db.transaction('rw', [dbTable], async () => {
+    const record = await dbTable.where('id').equals(data.id).modify(data);
     return record;
   });
 }
